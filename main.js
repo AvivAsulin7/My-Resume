5042dd54dd83ee4fbd4b7e750d8b278e999c22c7
אביב אסולין <avivasulin1211@gmail.com>
2022-05-26 15:38:01 +0300

    add project

--- main.js
+++ main.js
@@ -5,27 +5,27 @@ function clear_field() {
   text.value = "";
 }
 
-// const b = document.querySelector("#clear");
-// b.addEventListener("click", clear_field);
+const b = document.querySelector("#clear");
+b.addEventListener("click", clear_field);
 
-// // **************************************************
+// **************************************************
 
-// function sendEmail() {
-//   Email.send({
-//     Host: "smtp.gmail.com",
-//     Username: "avivasulin1211@gmail.com",
-//     Password: "jxhavylbcfeprndu",
-//     To: "avivasulin1211@gmail.com",
-//     From: document.getElementById("email").value,
-//     Subject: "New Contact",
-//     Body:
-//       "Name: " +
-//       document.getElementById("name").value +
-//       "<br> Email: " +
-//       document.getElementById("email").value +
-//       "<br> Phone no: " +
-//       document.getElementById("phone").value +
-//       "<br> Message: " +
-//       document.getElementById("message").value,
-//   }).then((message) => alert("Message sent to me , Thank you!"));
-// }
+function sendEmail() {
+  Email.send({
+    Host: "smtp.gmail.com",
+    Username: "avivasulin1211@gmail.com",
+    Password: "jxhavylbcfeprndu",
+    To: "avivasulin1211@gmail.com",
+    From: document.getElementById("email").value,
+    Subject: "New Contact from PROJECT:My Resume",
+    Body:
+      "Name: " +
+      document.getElementById("name").value +
+      "<br> Email: " +
+      document.getElementById("email").value +
+      "<br> Phone no: " +
+      document.getElementById("phone").value +
+      "<br> Message: " +
+      document.getElementById("message").value,
+  }).then((message) => alert("Message sent to me , Thank you!"));
+}
